9a46672e2763da18b59b63f587fddad352a5b572
Robert Faulkner <quivet@gmail.com>
2022-04-23 14:26:13 +0100

    displaying data

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App)({
+    data() {
+        return {
+            title: "Isles of Darkness",
+            copyright: "&copy; 2022."
+        }
+    }
+})
 
 app.use(router)
 
